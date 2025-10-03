a3248144e758cdbe4912726d01d73c6cecf65e7e
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-10-03 10:46:00 +0200

    Force light theme

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,14 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { PaperProvider } from 'react-native-paper';
+import { MD3LightTheme, PaperProvider } from 'react-native-paper';
 import HomeScreen from './screens/HomeScreen';
 import { ScrollView } from 'react-native';
+
+const theme = {
+  ...MD3LightTheme,
+  colors: {
+    ...MD3LightTheme.colors,
+    background: '#ffffff',
+    surface: '#ffffff',
+  },
+};
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
-    <PaperProvider>
+    <PaperProvider theme={theme}>
       <ScrollView style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
         <NavigationContainer>
           <Stack.Navigator>
