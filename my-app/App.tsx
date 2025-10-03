dba9ddf069ad03228eaa7b319b2e358de0dbf172
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-10-03 10:11:51 +0200

    Implement screens

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -1,11 +1,19 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
-import { AppRegistry } from 'react-native';
 import { PaperProvider } from 'react-native-paper';
+import HomeScreen from './screens/HomeScreen';
 
-export default function Main() {
+const Stack = createNativeStackNavigator();
+export default function App() {
   return (
     <PaperProvider>
-      ...
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PaperProvider>
   );
 }
+
