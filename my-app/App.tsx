cc1755fca664e2ebfd1f0235ae12c411f5b75902
Ivan Copeland <ivan.copeland2015@gmail.com>
2025-10-03 10:21:40 +0200

    Make app scrollable

--- my-app/App.tsx
+++ my-app/App.tsx
@@ -3,16 +3,19 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as React from 'react';
 import { PaperProvider } from 'react-native-paper';
 import HomeScreen from './screens/HomeScreen';
+import { ScrollView } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <PaperProvider>
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen name="Home" component={HomeScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
+      <ScrollView style={{ flex: 1, backgroundColor: '#f2f2f2' }}>
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </ScrollView>
     </PaperProvider>
   );
 }
